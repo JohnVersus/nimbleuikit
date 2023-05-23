9b53575fc9bc43820d4588b51a49246d4a5ff557
John Versus <johnvs.nagendra@gmail.com>
2023-05-23 23:59:06 +0530

    test: workflow fix 🧪

--- packages/atoms/src/Text/Text.stories.tsx
+++ packages/atoms/src/Text/Text.stories.tsx
@@ -64,7 +64,7 @@ export const LargeText: StoryObj<TextProps> = {
 LargeText.storyName = "Large Text";
 
 /**
- * A text component with bold text.
+ * A text component with bold text font.
  */
 export const BoldText: StoryObj<TextProps> = {
   args: {
