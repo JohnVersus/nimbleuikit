fd6b807d482f90ae89e270f238d58cf00e36eaef
JohnVersus <JohnVersus@users.noreply.github.com>
2023-06-20 07:26:48 +0000

    deploy: 9ca995d6be726c18c0e0cdb4e04eed16cd48f316

---
+++ assets/Header.stories-a6ddf567.js
@@ -0,0 +1,10 @@
+import{j as n}from"./jsx-runtime-91a467a5.js";import{H as d}from"./Header-f221caa9.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const l={title:"Atoms/Utils/Header",component:d,tags:["autodocs"],argTypes:{children:{control:"text",description:"The content of the header."}},parameters:{docs:{description:{component:"\n          The `Header` component is a simple utility component that wraps its children \n    inside a standard HTML `header` tag.\n        "},source:{code:`
+          <Header>
+            <h1>Example Page</h1>
+          </Header>
+        `}}}},e={args:{children:n("h1",{children:"Example Page"})}};e.storyName="Default Header";var a,r,t,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: <h1>Example Page</h1>
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source},description:{story:"### Usage\n```jsx\n<Header>\n<h1>Example Page</h1>\n</Header>\n```",...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.description}}};const h=["Default"];export{e as Default,h as __namedExportsOrder,l as default};
+//# sourceMappingURL=Header.stories-a6ddf567.js.map
