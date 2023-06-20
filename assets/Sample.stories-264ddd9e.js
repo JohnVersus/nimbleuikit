e449cf18177d4df5d7d00356a753293cf34889c2
JohnVersus <JohnVersus@users.noreply.github.com>
2023-06-20 07:22:12 +0000

    deploy: a41c899b3bb17f99fbbfc9f18c8e6bd928100d9d

--- assets/Sample.stories-264ddd9e.js
+++
@@ -1,9 +0,0 @@
-import{s as l,c as p}from"./system-af6bb858.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const a=l.div({},...p);try{a.displayName="Sample",a.__docgenInfo={description:`A Sample component.
-
-This component is a styled div, that can accept all props which a normal div can accept.`,displayName:"Sample",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: HTMLDivElement | null) => void) | RefObject<HTMLDivElement> | null"}},as:{defaultValue:null,description:"The HTML element or custom component as which the component should render",name:"as",required:!1,type:{name:"undefined"}},css:{defaultValue:null,description:`Custom CSS for the component.
-This can either be a CSSObject, or a function that receives the theme and returns a CSSObject.`,name:"css",required:!1,type:{name:"CSSObject | ((theme: any) => CSSObject)"}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"undefined"}}}}}catch{}const u={title:"Sample/Sample for devs",component:a,tags:["autodocs"],parameters:{docs:{description:{component:"Refer to [Props Details](?path=/docs/propsdetails--documentation) for details on the available props."}}}},e={args:{children:"Simple Sample Component"}};e.storyName="Simple Sample";var t,n,o,r,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "Simple Sample Component"
-  }
-}`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source},description:{story:"Simple Sample",...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.description}}};const S=["SimpleSample"];export{e as SimpleSample,S as __namedExportsOrder,u as default};
-//# sourceMappingURL=Sample.stories-264ddd9e.js.map
--- assets/preview-4cec3cc4.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-3ee3298e.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-d2b0793a.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-d2b0793a.js","./iframe-3ee3298e.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-4cec3cc4.js.map
