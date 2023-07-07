d0bdfc63668a64d2916608f3c004201e76ffe95e
John Versus <johnvs.nagendra@gmail.com>
2023-07-07 21:49:44 +0530

    Revert "Revert "chores: added react import line 🧹""
    This reverts commit 8e6ab64ba655306bbb1643b4ac74bcda16febee6.

--- packages/atoms/src/lib/FlexBox/FlexBox.stories.tsx
+++ packages/atoms/src/lib/FlexBox/FlexBox.stories.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { FlexBox, FlexBoxProps } from ".";
 import { Box } from "../Box";
--- packages/atoms/src/lib/Tooltip/Tooltip.stories.tsx
+++ packages/atoms/src/lib/Tooltip/Tooltip.stories.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Tooltip, TooltipProps } from ".";
 import { Button } from "../Button";
