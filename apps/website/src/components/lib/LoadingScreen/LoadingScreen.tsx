9884dc71ff5b763bba4161dd5c7c1cd131a85b06
John Versus <johnvs.nagendra@gmail.com>
2023-07-20 19:56:48 +0530

    chores: to optimise seo 🧑‍🔧

--- apps/website/src/components/lib/LoadingScreen/LoadingScreen.tsx
+++ apps/website/src/components/lib/LoadingScreen/LoadingScreen.tsx
@@ -13,19 +13,6 @@ const LoadingScreen = () => {
       justifyContent={"center"}
       alignItems={"center"}
       backgroundColor={theme.colors.primary}
-      css={{
-        "::before": {
-          content: '""',
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: 'url("/noise.webp")',
-          opacity: 0.4,
-          pointerEvents: "none",
-        },
-      }}
     >
       <Image
         src="/logo-loading.svg"
