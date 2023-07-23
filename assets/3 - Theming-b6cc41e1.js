8c7b27583dfaae799cbdde43b6758c83f1641832
JohnVersus <JohnVersus@users.noreply.github.com>
2023-07-23 04:26:43 +0000

    deploy: 107cf0861fbdf101411227e50672b5bf0b9579c8

---
+++ assets/3 - Theming-b6cc41e1.js
@@ -0,0 +1,49 @@
+import{j as r,a as n,F as c}from"./jsx-runtime-29545a09.js";import{u as d}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";function i(o){const e=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",pre:"pre",ul:"ul",li:"li",a:"a"},d(),o.components);return n(c,{children:[r(e.h1,{id:"theme",children:"Theme"}),`
+`,r(e.p,{children:"A theme in styled-components is a way of specifying common styles, such as colors, font sizes, and spacings, which can be shared and reused throughout the app. This is especially useful for design systems and maintaining consistency across the app."}),`
+`,r(e.h2,{id:"basic-usage",children:"Basic Usage"}),`
+`,n(e.p,{children:["First, import the ",r(e.code,{children:"ThemeProvider"})," from ",r(e.code,{children:"@nimbleuikit/atoms/utils"}),"."]}),`
+`,r(e.pre,{children:r(e.code,{className:"language-jsx",children:`import { ThemeProvider } from "@nimbleuikit/atoms/utils";
+`})}),`
+`,r(e.p,{children:"Then, define your theme:"}),`
+`,r(e.pre,{children:r(e.code,{className:"language-jsx",children:`const theme = {
+  colors: {
+    primary: "#6200ee",
+    secondary: "#03dac6",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32],
+};
+`})}),`
+`,n(e.p,{children:["Now, you can use ",r(e.code,{children:"ThemeProvider"})," to provide this theme to your components:"]}),`
+`,r(e.pre,{children:r(e.code,{className:"language-jsx",children:`import { Box } from "@nimbleuikit/atoms";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Box color="primary">This is a box</Box>
+    </ThemeProvider>
+  );
+}
+`})}),`
+`,n(e.p,{children:["In this example, the color of the text in the ",r(e.code,{children:"Box"})," component will be ",r(e.code,{children:"#6200ee"}),", which is the ",r(e.code,{children:"primary"})," color defined in the theme."]}),`
+`,r(e.h2,{id:"theme-structure",children:"Theme Structure"}),`
+`,r(e.p,{children:"The theme object consists of different properties, each representing a set of design tokens or values."}),`
+`,r(e.p,{children:"Here's an overview of the common properties:"}),`
+`,n(e.ul,{children:[`
+`,n(e.li,{children:[r(e.code,{children:"space"}),": Used for margin and padding."]}),`
+`,n(e.li,{children:[r(e.code,{children:"fontSizes"}),": Used for the ",r(e.code,{children:"font-size"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"colors"}),": Used for color-related properties, e.g., ",r(e.code,{children:"color"}),", ",r(e.code,{children:"background-color"}),", ",r(e.code,{children:"border-color"}),"."]}),`
+`,n(e.li,{children:[r(e.code,{children:"fonts"}),": Used for the ",r(e.code,{children:"font-family"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"fontWeights"}),": Used for the ",r(e.code,{children:"font-weight"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"lineHeights"}),": Used for the ",r(e.code,{children:"line-height"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"letterSpacings"}),": Used for the ",r(e.code,{children:"letter-spacing"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"sizes"}),": Used for width and height-related properties."]}),`
+`,n(e.li,{children:[r(e.code,{children:"borders"}),": Used for border-related properties."]}),`
+`,n(e.li,{children:[r(e.code,{children:"borderWidths"}),": Used for the ",r(e.code,{children:"border-width"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"borderStyles"}),": Used for the ",r(e.code,{children:"border-style"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"radii"}),": Used for the ",r(e.code,{children:"border-radius"})," property."]}),`
+`,n(e.li,{children:[r(e.code,{children:"shadows"}),": Used for shadow-related properties, e.g., ",r(e.code,{children:"box-shadow"}),", ",r(e.code,{children:"text-shadow"}),"."]}),`
+`,n(e.li,{children:[r(e.code,{children:"zIndices"}),": Used for the ",r(e.code,{children:"z-index"})," property."]}),`
+`]}),`
+`,n(e.p,{children:["In addition to these, you can also define style variants for specific components, like buttons or text, under their respective keys (",r(e.code,{children:"buttons"}),", ",r(e.code,{children:"textStyles"}),", etc.)"]}),`
+`,n(e.p,{children:["For more detailed information on the theme specification, please refer to the ",r(e.a,{href:"https://styled-system.com/theme-specification",target:"_blank",rel:"nofollow noopener noreferrer",children:"Styled System Theme Specification"}),"."]})]})}function a(o={}){const{wrapper:e}=Object.assign({},d(),o.components);return e?r(e,Object.assign({},o,{children:r(i,o)})):i(o)}export{a as default};
+//# sourceMappingURL=3 - Theming-b6cc41e1.js.map
---
+++ assets/TextArea.stories-80f49541.js
@@ -0,0 +1,24 @@
+import{T as g}from"./TextArea-0b4452d1.js";import"./system-aa5c237b.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const w={title:"Atoms/TextArea",component:g,tags:["autodocs"],argTypes:{rows:{control:"number"},cols:{control:"number"}},parameters:{docs:{description:{component:"Refer to [Props Details](?path=/docs/propsdetails--documentation) for details on the available props."}}}},e={args:{p:2,children:"Simple Text Area"}};e.storyName="Simple Text Area";const r={args:{p:2,rows:5,cols:80,children:"Customized Text Area"}};r.storyName="Customized Text Area";const s={args:{p:2,rows:5,cols:80,css:{resize:"none"},children:"Non Resizable Text Area"}};s.storyName="Non Resizable Text Area";var o,a,t,n,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    p: 2,
+    children: "Simple Text Area"
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source},description:{story:"Simple text area example.",...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.description}}};var c,p,m,d,l;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    p: 2,
+    rows: 5,
+    cols: 80,
+    children: "Customized Text Area"
+  }
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source},description:{story:"Customized text area example with rows and columns dimensions.",...(l=(d=r.parameters)==null?void 0:d.docs)==null?void 0:l.description}}};var x,u,T,A,z;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    p: 2,
+    rows: 5,
+    cols: 80,
+    css: {
+      resize: "none"
+    },
+    children: "Non Resizable Text Area"
+  }
+}`,...(T=(u=s.parameters)==null?void 0:u.docs)==null?void 0:T.source},description:{story:"Non resizable text area example.",...(z=(A=s.parameters)==null?void 0:A.docs)==null?void 0:z.description}}};const y=["SimpleTextArea","CustomizedTextArea","NonResizableTextArea"];export{r as CustomizedTextArea,s as NonResizableTextArea,e as SimpleTextArea,y as __namedExportsOrder,w as default};
+//# sourceMappingURL=TextArea.stories-80f49541.js.map
